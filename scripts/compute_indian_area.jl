52fb32095c31db0b696042b7d943a637f9df59bf
G Jake Gebbie <ggebbie@whoi.edu>
2022-04-24 18:13:27 -0400

    `indianarea` using 4 X 4 TMI

---
+++ scripts/compute_indian_area.jl
@@ -0,0 +1,11 @@
+# get basinwide mean values for the Indian Ocean
+
+# activate environment with DrWatson
+include("intro.jl")
+
+using Revise
+using HistoricalIndianOcean, DrWatson, TMI
+
+latbdy = -30
+area = indianarea(latbdy)
+
